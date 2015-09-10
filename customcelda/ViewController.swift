b64a10ff7f596f31cd1fee473e8475da515cb0df
manuelzegarra <mzegarra@gmail.com>
2015-09-10 02:04:19 -0500

    Initial Commit

---
+++ customcelda/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  customcelda
+//
+//  Created by Manuel Zegarra on 10/09/15.
+//  Copyright (c) 2015 M-Sonic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
